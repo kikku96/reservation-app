6c72b18d4a9ed7ed0c901faafbe68f3d28f4dbf4
kikku <owner@oto-sound.jp>
2021-01-24 15:48:03 +0900

    94,動的ページを作ろう

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+
+  products: any = [1, 2, 3, 4]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
