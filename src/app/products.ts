de13498a1acef7fd1818f518eef62907f8ddee85
kikku <owner@oto-sound.jp>
2021-01-24 18:15:27 +0900

    angular 106 フロントエンド側作成

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,50 @@
 export const products = [
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone XL',
         price: 799,
-        description: 'A large phone with one of the best screens'
+        description: 'A large phone with one of the best screens',
+        heding1: 'サンプルテキスト1',
+        heding2: 'サンプルテキスト2',
+        heding3: 'サンプルテキスト3',
+        headingtext1: 'サンプル説明1',
+        headingtext2: 'サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2',
+        headingtext3: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula p,'
     },
     {
+        coverImage: './assets/img/phone-cover.jpg',
         name: 'Phone Mini',
         price: 699,
-        description: 'A large phone with one of the best screens'
+        description: 'A large phone with one of the best screens',
+        heding1: 'サンプルテキスト1',
+        heding2: 'サンプルテキスト2',
+        heding3: 'サンプルテキスト3',
+        headingtext1: 'サンプル説明1',
+        headingtext2: 'サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2',
+        headingtext3: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula p,'
     },
     {
-        name: 'Phone XL',
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone standard',
         price: 299,
-        description: ''
+        description: '',
+        heding1: 'サンプルテキスト1',
+        heding2: 'サンプルテキスト2',
+        heding3: 'サンプルテキスト3',
+        headingtext1: 'サンプル説明1',
+        headingtext2: 'サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2',
+        headingtext3: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula p,'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: '',
+        heding1: 'サンプルテキスト1',
+        heding2: 'サンプルテキスト2',
+        heding3: 'サンプルテキスト3',
+        headingtext1: 'サンプル説明1',
+        headingtext2: 'サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2 サンプル説明2',
+        headingtext3: 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula p,'
     }
 ];
